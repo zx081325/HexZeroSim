6d4b2eedffcc7a1ed7b2a2db2e2741d38464f0f8
zx081325 <zx081325@163.com>
2021-11-14 18:21:49 +0800

    first commit

---
+++ simulate.py
@@ -0,0 +1,148 @@
+import time
+
+import requests
+import threading
+import torch
+import pickle
+from arguments import parser
+from models import Model
+from utils import create_buffers, act, get_batch
+from torch import multiprocessing as mp
+import zlib
+import time
+
+
+class Simulate(object):
+
+    def __init__(self, flags):
+        self.flags = flags
+        self.url_w = "http://zx081325.xyz:82/hex/white_weights.ckpt"
+        self.url_b = "http://zx081325.xyz:82/hex/black_weights.ckpt"
+        self.model_path_w = 'model/white_weights.ckpt'
+        self.model_path_b = 'model/black_weights.ckpt'
+        self.model = Model(device='cpu', board_size=flags.board_size)
+
+    # 从云盘下载模型
+    def download_pkl(self):
+        try:
+            res = requests.get(self.url_w, stream=True)
+            with open(self.model_path_w, "wb") as f:
+                for chunk in res.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            print("white模型下载成功")
+            res = requests.get(self.url_b, stream=True)
+            with open(self.model_path_b, "wb") as f:
+                for chunk in res.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            print("black模型下载成功")
+        except Exception as e:
+            print("下载模型失败:", repr(e))
+
+    # 加载模型
+    def load_model(self):
+        from models import get_model_dict
+        for position in ['white', 'black']:
+            model_dict = get_model_dict(self.flags.board_size)
+            model = model_dict[position]()
+            if position == "white":
+                model_state_dict = torch.load(self.model_path_w, map_location='cpu')
+            else:
+                model_state_dict = torch.load(self.model_path_b, map_location='cpu')
+            model.load_state_dict(model_state_dict)
+            model.eval()
+            self.model.models[position] = model
+            print("加载" + position + "模型成功")
+
+    # 生成对局数据
+    def simulate(self):
+        if not self.flags.actor_device_cpu:
+            if not torch.cuda.is_available():
+                raise AssertionError(
+                    "CUDA not available. If you have GPUs, please specify the ID after `--gpu_devices`. Otherwise, please train with CPU with `python3 train.py --actor_device_cpu --training_device cpu`")
+
+        if self.flags.actor_device_cpu:
+            device_iterator = ['cpu']
+        else:
+            device_iterator = range(self.flags.num_actor_devices)
+            assert self.flags.num_actor_devices <= len(self.flags.gpu_devices.split(
+                ',')), 'The number of actor devices can not exceed the number of available devices'
+
+        # 初始化buffers
+        buffers = create_buffers(self.flags, device_iterator)
+
+        # Initialize queues
+        actor_processes = []
+        ctx = mp.get_context('spawn')
+        free_queue = {}
+        full_queue = {}
+
+        for device in device_iterator:
+            _free_queue = {'white': ctx.SimpleQueue(), 'black': ctx.SimpleQueue()}
+            _full_queue = {'white': ctx.SimpleQueue(), 'black': ctx.SimpleQueue()}
+            free_queue[device] = _free_queue
+            full_queue[device] = _full_queue
+
+        for device in device_iterator:
+            for m in range(self.flags.num_buffers):
+                free_queue[device]['white'].put(m)
+                free_queue[device]['black'].put(m)
+
+        # Starting actor processes
+        for device in device_iterator:
+            for i in range(self.flags.num_actors):
+                actor = ctx.Process(
+                    target=act,
+                    args=(
+                    i, device, free_queue[device], full_queue[device], self.model, buffers[device], self.flags))
+                actor.start()
+                actor_processes.append(actor)
+
+        def batch_and_send(i, device, position, local_lock, lock=threading.Lock()):
+            """Thread target for the learning process."""
+            start_time = time.time()
+            while True:
+                batch = get_batch(free_queue[device][position], full_queue[device][position], buffers[device][position],
+                                  self.flags, local_lock)
+                with lock:
+                    # 先序列化，再压缩
+                    batch = zlib.compress(pickle.dumps(batch), level=9)
+                    # 发送batch
+                    data = {"file": batch}
+                    requests.post(url='http://zx081325.xyz:83/{}_batch'.format(position), files=data)
+                    print(position, "batch发送成功")
+
+                    end_time = time.time()
+                    print("time", end_time - start_time)
+                    if end_time - start_time > 60 and position == "white":
+                        # 更新模型
+                        self.download_pkl()
+                        self.load_model()
+                        start_time = time.time()
+
+        threads = []
+        locks = {}
+        for device in device_iterator:
+            locks[device] = {'white': threading.Lock(), 'black': threading.Lock()}
+
+        for device in device_iterator:
+            for i in range(self.flags.num_threads):
+                for position in ['white', 'black']:
+                    thread = threading.Thread(
+                        target=batch_and_send, name='batch-%d' % i,
+                        args=(i, device, position, locks[device][position]))
+                    thread.start()
+                    threads.append(thread)
+
+
+if __name__ == "__main__":
+    flags = parser.parse_args(['--actor_device_cpu'])
+    simulate = Simulate(flags)
+    simulate.download_pkl()
+    simulate.load_model()
+    simulate.simulate()
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,155 @@
+import torch
+import typing
+import logging
+import traceback
+from env import Env
+from env_utils import Environment
+
+Buffers = typing.Dict[str, typing.List[torch.Tensor]]
+
+shandle = logging.StreamHandler()
+shandle.setFormatter(
+    logging.Formatter(
+        '[%(levelname)s:%(process)d %(module)s:%(lineno)d %(asctime)s] '
+        '%(message)s'))
+log = logging.getLogger('hexzero')
+log.propagate = False
+log.addHandler(shandle)
+log.setLevel(logging.INFO)
+
+
+def create_env(flags):
+    return Env(flags.chess_num)
+
+
+def get_batch(free_queue,
+              full_queue,
+              buffers,
+              flags,
+              lock):
+    """
+    This function will sample a batch from the buffers based
+    on the indices received from the full queue. It will also
+    free the indices by sending it to full_queue.
+    """
+    with lock:
+        # full_queue 出队
+        indices = [full_queue.get() for _ in range(flags.batch_size)]
+        print(indices)
+    batch = {
+        key: torch.stack([buffers[key][m] for m in indices], dim=1)
+        for key in buffers
+    }
+    for m in indices:
+        # free_queue 入队
+        free_queue.put(m)
+    return batch
+
+
+def create_buffers(flags, device_iterator):
+    """
+    为不同的位置以及不同的设备(如GPU)创建缓冲区。也就是说，每个设备对于两个位置都有3倍长度作为特征值
+    """
+    T = flags.unroll_length  # 展开长度
+    positions = ['white', 'black']
+    buffers = {}
+    for device in device_iterator:  # 每个设备
+        buffers[device] = {}
+        for position in positions:  # 每个位置
+            x_dim = flags.chess_num * 3
+            specs = dict(
+                done=dict(size=(T,), dtype=torch.bool),
+                episode_return=dict(size=(T,), dtype=torch.float32),
+                target=dict(size=(T,), dtype=torch.float32),
+                obs_x=dict(size=(T, x_dim), dtype=torch.int8),
+            )
+            _buffers: Buffers = {key: [] for key in specs}
+            for _ in range(flags.num_buffers):  # num_buffers:50
+                for key in _buffers:
+                    if not device == "cpu":
+                        _buffer = torch.empty(**specs[key]).to(torch.device('cuda:' + str(device))).share_memory_()
+                    else:
+                        _buffer = torch.empty(**specs[key]).to(torch.device('cpu')).share_memory_()
+                    _buffers[key].append(_buffer)
+            buffers[device][position] = _buffers
+    return buffers
+
+
+def act(i, device, free_queue, full_queue, model, buffers, flags):
+    """
+    This function will run forever until we stop it. It will generate
+    data from the environment and send the data to buffer. It uses
+    a free queue and full queue to syncup with the main process.
+    """
+    positions = ['white', 'black']
+    try:
+        T = flags.unroll_length
+        log.info('Device %s Actor %i started.', str(device), i)
+
+        env = create_env(flags)
+        env = Environment(env, device)
+
+        done_buf = {p: [] for p in positions}
+        episode_return_buf = {p: [] for p in positions}
+        target_buf = {p: [] for p in positions}
+        obs_x_buf = {p: [] for p in positions}
+        size = {p: 0 for p in positions}
+
+        position, obs, env_output = env.initial()
+
+        while True:
+            while True:
+                with torch.no_grad():
+                    agent_output = model.forward(position, obs['x_batch'], flags=flags)
+                _action_idx = int(agent_output['action'].cpu().detach().numpy())
+                action = obs['legal_actions'][_action_idx]
+                env_output['obs_x_no_action'][action * 3] = 0
+                if position == "white":
+                    env_output['obs_x_no_action'][action * 3 + 1] = 1
+                else:
+                    env_output['obs_x_no_action'][action * 3 + 2] = 1
+                obs_x_buf[position].append(env_output['obs_x_no_action'])
+                size[position] += 1
+                position, obs, env_output = env.step(action)
+
+                if env_output['done']:
+                    for p in positions:
+                        diff = size[p] - len(target_buf[p])
+                        if diff > 0:
+                            done_buf[p].extend([False for _ in range(diff - 1)])
+                            done_buf[p].append(True)
+
+                            episode_return = env_output['episode_return'] if p == 'white' else -env_output[
+                                'episode_return']
+                            episode_return_buf[p].extend([0.0 for _ in range(diff - 1)])
+                            episode_return_buf[p].append(episode_return)
+                            target_buf[p].extend([episode_return for _ in range(diff)])
+                    break
+
+            for p in positions:
+                while size[p] > T:
+                    # free_queue出队
+                    index = free_queue[p].get()
+                    print("index", index)
+                    if index is None:
+                        break
+                    for t in range(T):
+                        buffers[p]['done'][index][t, ...] = done_buf[p][t]
+                        buffers[p]['episode_return'][index][t, ...] = episode_return_buf[p][t]
+                        buffers[p]['target'][index][t, ...] = target_buf[p][t]
+                        buffers[p]['obs_x'][index][t, ...] = obs_x_buf[p][t]
+                    # full_queue入队
+                    full_queue[p].put(index)
+                    done_buf[p] = done_buf[p][T:]
+                    episode_return_buf[p] = episode_return_buf[p][T:]
+                    target_buf[p] = target_buf[p][T:]
+                    obs_x_buf[p] = obs_x_buf[p][T:]
+                    size[p] -= T
+
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        log.error('Exception in worker process %i', i)
+        traceback.print_exc()
+        print()
+        raise e
